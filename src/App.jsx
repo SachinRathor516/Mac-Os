199ce8633a9d76e9feb1086508d7a1b78d4f174d
Sachin Rathor <sachinrathor516@gamil.com>
2026-06-08 11:39:55 +0530

    Dock effect

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './app.scss'
+import Dock from './componants/Dock'
+
+function App() {
+
+  return (
+    <main>
+      <Dock/>
+    </main>
+  )
+}
+
+export default App
